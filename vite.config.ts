64f15c4e27f5b5047329c79da031e30bab5350f6
Alejandro Oliva <alejandro.oliva2409@gmail.com>
2023-09-17 01:06:49 -0300

    refactor: path layout added

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
 		alias: {
 			'@': path.resolve(__dirname, './src'),
 			'@components': path.resolve(__dirname, './src/components'),
+			'@layouts': path.resolve(__dirname, './src/layouts'),
 			'@pages': path.resolve(__dirname, './src/pages'),
 			'@redux': path.resolve(__dirname, './src/redux'),
 			'@routes': path.resolve(__dirname, './src/routes'),
