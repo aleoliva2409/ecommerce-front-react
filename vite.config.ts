de7cebdc2b8234f64c8cbb7d8cd0b3c4742b61e6
Alejandro Oliva <alejandro.oliva2409@gmail.com>
2023-09-17 00:48:24 -0300

    build: routes and paths added

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+			'@components': path.resolve(__dirname, './src/components'),
+			'@pages': path.resolve(__dirname, './src/pages'),
+			'@redux': path.resolve(__dirname, './src/redux'),
+			'@routes': path.resolve(__dirname, './src/routes'),
+		},
+	},
+});
